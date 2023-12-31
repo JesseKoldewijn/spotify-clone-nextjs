bc4ede02ab562c305d6cf7648c755ae89592eeff
Zaprovic <jazaro2001@gmail.com>
2023-12-31 11:19:28 -0500

    changed spotify logo to not appear when there is active session

--- src/components/sidebar-header.tsx
+++ src/components/sidebar-header.tsx
@@ -1,12 +1,15 @@
 import HomeIcon from "@/icons/home-icon";
 import SearchIcon from "@/icons/search-icon";
 import SpotifyIcon from "@/icons/spotify-icon";
+import { useSession } from "next-auth/react";
 import SidebarHeaderItem from "./sidebar-header-item";
 
 const SidebarHeader = () => {
+	const { data: session } = useSession();
+
 	return (
 		<header className="bg-neutral-900 py-4 px-6 flex flex-col gap-5 items-start">
-			<SpotifyIcon />
+			{!session && <SpotifyIcon />}
 
 			<ul className="flex flex-col gap-5">
 				<SidebarHeaderItem name="Home">
