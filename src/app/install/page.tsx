6166980fcbf756e169957b3051106baa8ffd0ed4
Zaprovic <jazaro2001@gmail.com>
2023-12-31 13:54:29 -0500

    added /install page

---
+++ src/app/install/page.tsx
@@ -0,0 +1,9 @@
+const InstallPage = () => {
+	return (
+		<div>
+			<h1 className="text-5xl font-bold mt-4 text-center">Install page</h1>
+		</div>
+	);
+};
+
+export default InstallPage;
--- src/components/content-header-auth.tsx
+++ src/components/content-header-auth.tsx
@@ -1,6 +1,8 @@
 "use client";
+import InstallIcon from "@/icons/install-icon";
 import NotificationsIcon from "@/icons/notifications-icon";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import LoginLink from "./login-link";
 import ProfilePhotoButton from "./profile-photo";
 import SignUpLink from "./signup-link";
@@ -21,6 +23,12 @@ const ContentHeaderAuth = () => {
 			{session ? (
 				<>
 					<div className="flex items-center justify-center gap-1">
+						<Link href={"/install"}>
+							<button className="px-3 py-[0.3125rem] rounded-full flex gap-2 items-center bg-zinc-950 justify-center transition-all hover:scale-[1.03]">
+								<InstallIcon />
+								<h6 className="text-sm">Install app</h6>
+							</button>
+						</Link>
 						<button className="bg-zinc-950 p-2 rounded-full transition-transform hover:scale-105 text-zinc-400 hover:text-zinc-100">
 							<NotificationsIcon />
 						</button>
