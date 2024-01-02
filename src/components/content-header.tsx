b676ca00b2cb5b1001c4082d28243aa6097452f2
Zaprovic <jazaro2001@gmail.com>
2024-01-01 23:05:58 -0500

    added styling to content header:

--- src/components/content-header.tsx
+++ src/components/content-header.tsx
@@ -13,20 +13,20 @@ const ContentHeader = () => {
 		<header
 			id="content-header"
 			className={`${
-				session ? "py-3" : "py-2 bg-zinc-900"
+				session ? "py-3" : "py-2 bg-neutral-950"
 			} sticky top-0 bg-zinc-900`}
 		>
 			<div className=" flex items-center justify-between px-6">
 				<div className="flex gap-2">
 					<button
 						onClick={() => router.back()}
-						className="grid place-items-center text-zinc-100 bg-zinc-950 p-2 aspect-square rounded-full"
+						className="grid place-items-center text-zinc-100 bg-black p-2 aspect-square rounded-full"
 					>
 						<ChevronLeft />
 					</button>
 					<button
 						onClick={() => router.forward()}
-						className="grid place-items-center text-zinc-100 bg-zinc-950 p-2 aspect-square rounded-full"
+						className="grid place-items-center text-zinc-100 bg-black p-2 aspect-square rounded-full"
 					>
 						<ChevronRight />
 					</button>
